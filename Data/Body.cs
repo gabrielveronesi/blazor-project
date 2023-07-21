63cbab773896b37e9ad09a18d7a91d49086ac771
Gabriel Veronesi <57780382+gabrielveronesi@users.noreply.github.com>
2023-07-20 23:57:28 -0300

    dev ok

---
+++ Data/Body.cs
@@ -0,0 +1,7 @@
+namespace blazor_project.Data;
+
+public class Body
+{
+    public string number { get; set; }
+    public string message { get; set; }
+}
---
+++ Data/Casa.cs
@@ -0,0 +1,7 @@
+namespace blazor_project.Data;
+
+public class Casa
+{
+    public int idCasa { get; set; }
+    public string titulo { get; set; }
+}
